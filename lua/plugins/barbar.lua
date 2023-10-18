7e73fc1b530dcd620925ac34bdb683a9b4165129
Wo-m <dom.thompson458@gmail.com>
2023-10-18 19:37:32 +1000

    autosave, autoclose, barbar, cleanup

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,20 @@
+return{
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    config = function()
+      local barbar = require('barbar')
+      barbar.setup()
+
+      local keymap = vim.keymap
+      keymap.set('n', '<Leader><', '<Cmd>BufferPrevious<CR>', opts)
+      keymap.set('n', '<Leader>>', '<Cmd>BufferNext<CR>', opts)
+      keymap.set('n', '<Leader>t', '<Cmd>BufferPick<CR>', opts)
+    end,
+    opts = {
+    },
+    version = '^1.0.0'
+}
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -12,7 +12,7 @@ return {
         -- import cmp-nvim-lsp plugin
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local keymap = vim.keymap -- for conciseness
+        local keymap = vim.keymap
 
         local opts = { noremap = true, silent = true }
         local on_attach = function(client, bufnr)
@@ -31,26 +31,11 @@ return {
             opts.desc = "Show LSP implementations"
             keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-            opts.desc = "Show LSP type definitions"
-            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
             opts.desc = "See available code actions"
             keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
             opts.desc = "Smart rename"
             keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-            opts.desc = "Show buffer diagnostics"
-            keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-            opts.desc = "Show line diagnostics"
-            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-            opts.desc = "Go to previous diagnostic"
-            keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-            opts.desc = "Go to next diagnostic"
-            keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
             opts.desc = "Show documentation for what is under cursor"
             keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
