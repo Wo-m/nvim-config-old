dff9b45b4effc854594b139e6eaf76f5a2b5b9ae
Wo-m <dom.thompson458@gmail.com>
2023-10-19 09:46:52 +1000

    quicknote

---
+++ lua/plugins/quicknote.lua
@@ -0,0 +1,16 @@
+return {
+    "RutaTang/quicknote.nvim",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    config=function()
+        local quicknote = require("quicknote")
+        quicknote.setup({})
+
+        local keymap = vim.keymap
+
+        -- Always use notes at CWD
+        keymap.set('n', '<Leader>nn', '<Cmd>lua require("quicknote").NewNoteAtCWD()<CR>', opts)
+        keymap.set('n', '<Leader>no', '<Cmd>lua require("quicknote").OpenNoteAtCWD()<CR>', opts)
+        keymap.set('n', '<Leader>nl', '<Cmd>lua require("quicknote").ListNotesForCWD()<CR>', opts)
+        keymap.set('n', '<Leader>nd', '<Cmd>lua require("quicknote").DeleteNoteAtCWD()<CR>', opts)
+    end,
+}
