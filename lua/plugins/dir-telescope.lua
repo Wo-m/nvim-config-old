18accdf37f6abf0e1d5c8c519161597fb1792986
Dominic Thompson <99559810+Wo-m@users.noreply.github.com>
2024-02-27 06:15:33 +1100

    dir-telescope

---
+++ lua/plugins/dir-telescope.lua
@@ -0,0 +1,3 @@
+return {
+    "princejoogie/dir-telescope.nvim"
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,27 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    telescope.load_extension("fzf")
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+        telescope.load_extension("fzf")
+        telescope.load_extension("dir")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+        -- set keymaps
+        local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    
-    -- Ensure you have ripgrep installed for live_grep to work
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-  end,
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+
+        -- Ensure you have ripgrep installed for live_grep to work
+        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+        -- Directory Search
+        keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<cr>", { desc = "Find string in directory" })
+    end,
 }
